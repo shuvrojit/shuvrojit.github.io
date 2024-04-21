86197565693353e0bc8b154bf0c4dfbb66ce1f90
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-04-21 17:54:15 +0600

    Add homepage components
    Still under development but something needs to be shown.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/header.tsx";
+import Header from "./components/header/header.tsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        <div className="main-layout">
-        {children}
-        </div>
+        <div className="main-layout">{children}</div>
       </body>
     </html>
   );
