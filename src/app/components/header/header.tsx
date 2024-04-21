c198f70cb59560287a7d5b215350abfdafc55f53
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-04-21 17:53:12 +0600

    Add header components
    add header nameplate and navigation bar which is not fully
    implemented yet.

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { useState } from "react";
+import HamburgerMenu from "./hamburger-menu";
+import styles from "./header.module.css";
+import Link from "next/link";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  function toggleMenu() {
+    setToggle(!toggle);
+  }
+  return (
+    <>
+      <div className={styles.header__container}>
+        <div className={styles.branding}>
+          <h1>Shuvrojit Biswas</h1>
+          <h2>Full Stack Developer & Desinger</h2>
+        </div>
+        <div
+          className={
+            (toggle ? " overlay__content " : "") +
+            styles.overlay +
+            " " +
+            styles.overlay__left
+          }
+        >
+          <Link href="/work">Work</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/music">Music</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contct">Contact</Link>
+        </div>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <HamburgerMenu direction="right" position="top-right" />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
