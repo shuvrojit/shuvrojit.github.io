589e425fe16d33386e5ba464f19e19cd37bd5f51
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-04-29 23:44:14 +0600

    Parse blog content, metadata and routes from mdx files
    I have added two functions. One for getting the file lists
    from directory and one for getting the file contents and
    metadata.

---
+++ src/lib/data.ts
@@ -0,0 +1,21 @@
+import fs from "fs";
+
+export function getMDXFiles(dirPath: string): string[] {
+  if (!fs.existsSync(dirPath)) {
+    return [];
+  }
+  const files = fs.readdirSync(dirPath);
+  files.forEach((file, index) => {
+    files[index] = file.replace(/\.mdx$/, "");
+  });
+  return files;
+}
+
+export async function getMDXFileBySlug(slug: string) {
+  try {
+    const mdxContent = await import(`@/content/${slug}.mdx`);
+    return mdxContent;
+  } catch (error) {
+    return error;
+  }
+}
