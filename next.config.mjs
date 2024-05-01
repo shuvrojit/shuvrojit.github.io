dc8a4aff4fea6d37384282bc9851e9992fe2a29f
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-05-02 03:14:08 +0600

    Fix the issues in the next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
+/** @type {import('next').NextConfig} */
 import createMDX from '@next/mdx'
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
   compiler: {
