ddde3f44decdbdb0231201582f90b14b1f2858ac
Shuvrojit Biswas <jsuvro17@gmail.com>
2024-06-05 11:50:34 +0600

    Fix contact component issues with responsive design
    I have fixed the responsive design issues that we were having
    over mobile screens. Changed the margins.Added new media
    settings.
    
    Changed send message button colors, borders
    
    Give the body a new background color.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ const ContactPage = () => {
   return (
     <>
       <ContactForm
-        formDescription="If you have any questions or concerns, please feel free to contact us. We will get back to you as soon as possible."
+        formDescription="If you have any questions or concerns, please feel free to contact me. I will get back to you as soon as possible."
         imageSrc="/images/contact.svg"
         imageAlt="Contact Us"
       />
